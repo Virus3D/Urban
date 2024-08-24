fce0a3109d6d4c651676edd029c9545d02a0076c
Virus3D <virus3d@rambler.ru>
2024-08-24 11:56:49 +0700

    module_3_1

---
+++ module3/module_3_1.py
@@ -0,0 +1,21 @@
+calls = 0
+def count_calls():
+    global calls
+    calls += 1
+
+def string_info(string):
+    count_calls()
+    return (len(string), string.upper(), string.lower())
+
+def is_contains(string, list):
+    count_calls()
+    for item in list:
+        if string.lower() == item.lower():
+            return True
+    return False
+
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
+print(calls)
