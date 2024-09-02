e10fc6feac9a3af8f20856aca9ea38dfc99e81a1
Virus3D <virus3d@rambler.ru>
2024-09-02 17:46:35 +0700

    module_5_2

---
+++ module5/module_5_2.py
@@ -0,0 +1,25 @@
+class House:
+    def __init__(self, name, number_of_floors) -> None:
+        self.name = name
+        self.number_of_floors = number_of_floors
+    def go_to(self, new_floor) -> None:
+        if new_floor < 1 or new_floor > self.number_of_floors:
+            print('Такого этажа не существует')
+            return
+        for floor in range(1, new_floor+1):
+            print(floor)
+    def __len__(self) -> int:
+        return self.number_of_floors
+    def __str__(self) -> str:
+        return f'Название: {self.name}, кол-во этажей: {self.number_of_floors}'
+
+h1 = House('ЖК Эльбрус', 10)
+h2 = House('ЖК Акация', 20)
+
+# __str__
+print(h1)
+print(h2)
+
+# __len__
+print(len(h1))
+print(len(h2))
