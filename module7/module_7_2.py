7094d0fd42751da5489651e212e422cc7ded149d
Virus3D <virus3d@rambler.ru>
2024-09-22 19:20:46 +0700

    module_7_2

---
+++ module7/module_7_2.py
@@ -0,0 +1,20 @@
+def custom_write(file_name: str, strings:list) -> tuple:
+    strings_positions = {}
+    file = open(file_name, 'w', encoding='utf-8')
+    for index, line in enumerate(strings):
+        tell = file.tell()
+        strings_positions[(index+1, tell)] = line
+        file.write(line + "\n")
+    file.close()
+    return strings_positions
+
+info = [
+    'Text for tell.',
+    'Используйте кодировку utf-8.',
+    'Because there are 2 languages!',
+    'Спасибо!'
+    ]
+
+result = custom_write('test.txt', info)
+for elem in result.items():
+  print(elem)
