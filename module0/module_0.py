41d36172d93638f3428cb08e2f480718adc2828d
Virus3D <viisto@yandex.ru>
2025-01-07 12:24:08 +0700

    module0

---
+++ module0/module_0.py
@@ -0,0 +1,21 @@
+# Задача 1 (просто) "Арифметика":
+
+# 1st program
+print(9 ** 0.5 * 5)
+
+# Задача 2 (просто) "Логика":
+# 2nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+
+# Задача 3 (средне) "Школьная загадка":
+# 3nd program
+first = 2 * 2 + 2
+second = 2 * (2 + 2)
+print(first)
+print(second)
+print(first == second)
+
+# Задача 4 (сложно) "Первый после точки":
+# 4th program
+str = '123.456'
+print(int(float(str) * 10) % 10)
