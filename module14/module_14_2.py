a30c7b8dbaea7512dba91af84fa8905d51a61bc7
Virus3D <viisto@yandex.ru>
2025-01-08 18:54:51 +0700

    module_14_2.py

--- module14/module_14_2.py
+++ module14/module_14_2.py
@@ -22,10 +22,16 @@ cursor.execute('UPDATE Users SET balance = 500 WHERE id % 2 != 0')
 
 cursor.execute('DELETE FROM Users WHERE id % 3 = 1')
 
-cursor.execute('SELECT username, email, age, balance FROM Users WHERE age != 60')
-users = cursor.fetchall()
-for user in users:
-    print(f'Имя: {user[0]} | Почта: {user[1]} | Возраст: {user[2]} | Баланс: {user[3]}')
+cursor.execute('DELETE FROM Users WHERE id = 6')
+
+cursor.execute('SELECT COUNT(*) FROM Users')
+total_users = cursor.fetchone()[0]
+
+cursor.execute('SELECT SUM(balance) FROM Users')
+all_balances = cursor.fetchone()[0]
+
+print(all_balances / total_users)
     
 connection.commit()
 connection.close()
+
